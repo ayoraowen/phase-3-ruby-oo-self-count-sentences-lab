03cce33228366e4029bb22664fe5c90e69333e5d
owenayora <ayoraowen@gmail.com>
2023-03-26 17:29:31 +0300

    Not yet completed count_sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,29 @@ require 'pry'
 
 class String
 
+  # attr_accessor :sentence
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    self.split
+    binding.pry
 
   end
 end
+
+c1 = String.new("one. two. three?")
+c1.count_sentences
+# c1.end_with?(".")
+
+# binding.pry
